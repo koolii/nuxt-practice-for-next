bc5766144dcd8f4977698ef38a21e151f20810c5
koolii <koolii0909@gmail.com>
2019-12-13 17:53:18 +0100

    add vuetify and edit minimum

--- vuesplash/resources/js/app.js
+++ vuesplash/resources/js/app.js
@@ -1,12 +1,19 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@mdi/font/css/materialdesignicons.css";
+
+Vue.use(Vuetify);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: {
     App
   },
-  template: '<App />'
+  template: "<App />"
 });
