0435f059faccfb18c4fe616d263eb49470df2b76
koolii <koolii0909@gmail.com>
2019-12-19 15:32:54 +0100

    add modal view named PhotoForm in Navbar.vue

---
+++ vuesplash/server/resources/js/components/PhotoForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- フォームを表示するかどうかを示す変数を v-model で管理 -->
+  <!-- v-showで管理 -->
+  <div v-show="value" class="photo-form">
+    <h2 class="title">Subimit a photo</h2>
+    <form class="form">
+      <input class="form__item" type="file" />
+      <div class="form__button">
+        <button type="submit" class="button button--inverse">submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  // valueを受け取れるようにpropsを追加
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
